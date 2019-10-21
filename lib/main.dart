8d0dd563e602d08e7b7f816a907fc3603f5e2617
Shivani Mehta <shivanimehta7n7@gmail.com>
2019-10-21 12:51:51 +0530

    Adding sounds and colors to xylophone interface

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int soundNo) {
+    final player = AudioCache();
+    player.play('note$soundNo.wav');
+  }
+
+  Widget buildButton(int soundNo, Color color) {
+    return Expanded(
+      child: FlatButton(
+        color: color,
+        onPressed: () {
+          playSound(soundNo);
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              buildButton(1, Colors.red),
+              buildButton(2, Colors.pink[300]),
+              buildButton(3, Colors.yellow),
+              buildButton(4, Colors.orange),
+              buildButton(5, Colors.greenAccent),
+              buildButton(6, Colors.green),
+              buildButton(7, Colors.blueAccent),
+            ],
+          ),
         ),
       ),
     );
